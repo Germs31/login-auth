7ac73514ee07f437ab2209920349888706f53907
German Nunez <german90nunez@gmail.com>
2020-05-21 13:55:30 -0700

    sessions added and flash for success message installed

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -9,8 +11,25 @@ require('./db/db');
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Body Parse
+// Body Parser
 app.use(express.urlencoded({ extended: false }));
+
+//  Express Session
+app.use(session({
+    secret: 'secret word',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Connect flash
+app.use(flash());
+
+// Global 
+app.use((req,res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+})
 
 // Routes
 app.use('/', require('./routes/index'));
