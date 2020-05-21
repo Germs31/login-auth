0299f1960e685c26b4402f92ee70661580d4b5ea
German Nunez <german90nunez@gmail.com>
2020-05-20 21:14:20 -0700

    user registeration adding to mongo db and styled errors with bootstrap alerts

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+// User Model
+const User = require('../models/User');
 
 router.get('/login', (req,res) => {
     res.render('login')
@@ -35,10 +39,45 @@ router.post('/register', (req, res) => {
             email,
             password,
             password2
-        })
+        });
     } else {
-        res.send('pass')
+        // Validation Passed
+        User.findOne({ email: email })
+            .then(user => {
+                if(user) {
+                    // User Exists 
+                    errors.push({ msg: 'Email is already registered' });
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password
+                    });
+                    // Hash Password
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if(err) throw err;
+                            // set password to hashed
+                            newUser.password = hash;
+
+                            // save user
+                            newUser.save()
+                                .then(user => {
+                                    res.redirect('/login');
+                                })
+                                .catch(err => console.log(err));
+                        })
+                    });
+                }
+            });
     }
-})
+});
 
 module.exports = router;
