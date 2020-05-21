ee164ca9a6fc53c041cbbb27e1df7f03b1e999e2
German Nunez <german90nunez@gmail.com>
2020-05-21 15:00:45 -0700

    dashboard protected

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth');
 // welcome
 router.get('/', (req,res) => {
     res.render('welcome');
 });
 // Dashboard
-router.get('/dashboard', (req,res) => {
+router.get('/dashboard', ensureAuthenticated, (req,res) => {
     res.render('dashboard');
 });
 
