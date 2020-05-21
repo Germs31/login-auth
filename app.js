5374e8d2421b9a031e1050c213406bcad58afd8f
German Nunez <german90nunez@gmail.com>
2020-05-21 14:41:41 -0700

    login authentication created with passport

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const flash = require('connect-flash');
 const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
+
+require('./config/passport')(passport);
 
 require('./db/db');
 
@@ -21,6 +24,10 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Connect flash
 app.use(flash());
 
@@ -28,6 +35,7 @@ app.use(flash());
 app.use((req,res, next) => {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
     next();
 })
 
